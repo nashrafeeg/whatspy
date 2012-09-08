14746e16e6ddfc78f6b198150f11e6cc365ad03a
Nash Rafeeq <nash.rafeeq@redtreeunwired.com>
2012-09-08 15:21:27 +0800

    set up the globals

---
+++ globals.py
@@ -0,0 +1,25 @@
+
+
+
+class Globals():
+	SERVER = 's.whatsapp.net'
+	HOST = 'bin-short.whatsapp.net'
+	DIGEST_URI = 'xmpp/s.whatsapp.net'
+	REALM = 's.whatsapp.net'
+	QOP = 'auth'
+	CONTENT_IDENTIFIER = 'STREAM_CLIENT_PERSISTENT' 
+	MSG_IDENTIFIER = "\x5D\x38\xFA\xFC"		
+	SERVER_DELIVERY_IDENTIFIER = "\x8C"		
+	CLENT_DELEVERY_IDENTIFIER = "\x7f\xbd\xad"
+	ACC_INFO_INDENT = "\x99\xBD\xA7\x94"		
+	LAST_SEEN_IDENT = "\x48\x38\xFA\xFC"
+	LAST_SEEN_IDENT2 = "\x7B\xBD\x4C\x8B"
+	Login_Data = "WA"+"\x00\x04\x00\x19\xf8\x05\x01\xa0\x8a\x84\xfc\x11"+"iPhone-2.6.9-5222" 
+	Login_Data = Login_Data +"""\x00\x08\xf8\x02\x96\xf8\x01\xf8\x01\x7e\x00\x07\xf8
+								\x05\x0f\x5a\x2a\xbd\xa7"""
+	
+	
+	
+	
+	
+	
